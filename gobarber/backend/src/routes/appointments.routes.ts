22c8711866fd7b3d4420f59fbe001bcc3dddde5e
guilherme <guilhermecardoso.info@gmail.com>
2020-06-27 13:42:01 -0300

    feat: Create appointment and user on Models

--- gobarber/backend/src/routes/appointments.routes.ts
+++ gobarber/backend/src/routes/appointments.routes.ts
@@ -1,27 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import AppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointmente = new AppointmentService(appointmentsRepository);
+    const createAppointmente = new AppointmentService();
 
-    const appointment = createAppointmente.execute({
+    const appointment = await createAppointmente.execute({
       date: parsedDate,
       provider,
     });
