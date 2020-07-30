544c1c416d09ffdeb517fc2ffe27281bd79cfeaa
guilherme <guilhermecardoso.info@gmail.com>
2020-07-30 19:15:10 -0300

    feat: page repo

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import api from '../../services/api';
 
-const Dasboard: React.FC = () => <Title>Dashboard</Title>;
+import logoImage from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dasboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    // Adição de um novo repositório
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+    console.log(response.data);
+  }
+
+  return (
+    <>
+      <img src={logoImage} alt="Github Explorer" />
+      <Title>Explore reposiórios no GitHub</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/15979107?s=460&u=6db9444e295e2d7222301541f12af526363bd745&v=4"
+            alt="Gui"
+          />
+          <div>
+            <strong>rockeseat/unform</strong>
+            <p>Uma descrição bem legal! para forms JS!</p>
+          </div>
+          <FiChevronRight size="20" />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dasboard;
